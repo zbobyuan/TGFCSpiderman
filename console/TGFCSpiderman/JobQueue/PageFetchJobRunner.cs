dfd191b54b357be780b242efe43c47d674be6a9e
bob <16703306@qq.com>
2015-01-02 00:19:38 +0800

    避免跑的太快引起发热。

--- console/TGFCSpiderman/JobQueue/PageFetchJobRunner.cs
+++ console/TGFCSpiderman/JobQueue/PageFetchJobRunner.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using taiyuanhitech.TGFCSpiderman.CommonLib;
 
@@ -9,7 +10,9 @@ namespace taiyuanhitech.TGFCSpiderman.JobQueue
 {
     public class PageFetchJobRunner : JobRunner<PageFetchRequest>
     {
+        private const int WaitingMultiplier = 2;//TODO:configurable
         private readonly IPageFetcher _pageFetcher;
+        private int _lastTimeRunningElapsedMilliseconds;
 
         public PageFetchJobRunner(IPageFetcher fetcher)
         {
@@ -23,10 +26,15 @@ namespace taiyuanhitech.TGFCSpiderman.JobQueue
 
         protected override void ExecuteJobs(List<PageFetchRequest> jobs)
         {
+            Console.WriteLine("等待{0:0.00}秒", _lastTimeRunningElapsedMilliseconds * WaitingMultiplier / 1000.00);
+            Thread.Sleep(_lastTimeRunningElapsedMilliseconds * WaitingMultiplier);
             jobs.ForEach(j => Console.WriteLine(j.HumanReadableDescription.ChangeStatusInDescription("正在获取")));
+            var sw = Stopwatch.StartNew();
             var tasks = jobs.Select(job => _pageFetcher.Fetch(job)
                             .ContinueWith(task => TaskQueueManager.Inst.OnPageFetchCompleted(task.Result))).ToArray();
             Task.WaitAll(tasks);
+            sw.Stop();
+            _lastTimeRunningElapsedMilliseconds = (int)sw.ElapsedMilliseconds;
         }
     }
 }
