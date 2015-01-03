5ec11b8af7cb95ce90ad86402f63701c4951aec3
bob <16703306@qq.com>
2015-01-03 22:54:36 +0800

    Get rid of useless using.

--- console/TGFCSpiderman/Program.cs
+++ console/TGFCSpiderman/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.FSharp.Core;
 using taiyuanhitech.TGFCSpiderman.Configuration;
 
 namespace taiyuanhitech.TGFCSpiderman
