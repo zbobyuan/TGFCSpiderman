9729fbc3ecab0fbefc725dae985f92c912640a98
Bob Yuan <16703306@qq.com>
2015-01-14 23:38:43 +0800

    use SQL statement "like" to implement search.

--- console/TGFCSpiderman/Persistence/IPostRepository.cs
+++ console/TGFCSpiderman/Persistence/IPostRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using taiyuanhitech.TGFCSpiderman.CommonLib;
 
 namespace taiyuanhitech.TGFCSpiderman.Persistence
@@ -6,5 +8,8 @@ namespace taiyuanhitech.TGFCSpiderman.Persistence
     public interface IPostRepository
     {
         void SavePosts(IEnumerable<Post> posts);
+
+        Task<List<PostWithThreadTitle>> SearchAsync(string userName, string title, string content, DateTime? beginTime,
+            DateTime? endTime, bool topicOnly, int pageSize, int pageNumber);
     }
 }
