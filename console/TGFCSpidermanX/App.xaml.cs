ff65e6150cd6685b3eca6066b49bb61bec005a1b
bob <16703306@qq.com>
2015-01-17 18:40:18 +0800

    single instance application

--- console/TGFCSpidermanX/App.xaml.cs
+++ console/TGFCSpidermanX/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using taiyuanhitech.TGFCSpiderman;
 using taiyuanhitech.TGFCSpiderman.CommonLib;
 
@@ -6,9 +8,15 @@ namespace taiyuanhitech.TGFCSpidermanX
 {
     public partial class App
     {
+        static readonly Mutex AppMutex = new Mutex(true, "{991D79D4-6DC1-4FD4-A5C5-C3DB3B17F237}");
         public RunningInfo RunningInfo { get; set; }
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (!AppMutex.WaitOne(TimeSpan.Zero, true)) 
+            {
+                MessageBox.Show("已经有一个在运行了，不能多开。");
+                Current.Shutdown();
+            }
             base.OnStartup(e);
             ComponentFactory.Startup();
         }
