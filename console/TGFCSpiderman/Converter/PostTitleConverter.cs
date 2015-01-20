4e49b19825794512373337f85f1ef674e0b985c9
bob <16703306@qq.com>
2015-01-20 15:09:30 +0800

    搜索结果中显示正确的标题和发表时间

---
+++ console/TGFCSpiderman/Converter/PostTitleConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace taiyuanhitech.TGFCSpiderman.Converter
+{
+    class PostTitleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue) return "";
+            var threadTitle = (string)values[0];
+            var order = (int)values[1];
+            return order == 1 ? threadTitle : "RE:" + threadTitle;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
